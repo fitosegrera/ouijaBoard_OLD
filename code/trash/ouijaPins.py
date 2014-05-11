fd87e5912230cc4b2f7fcc40170e878e88bbf4e2
fitosegrera <pi@raspberrypi.(none)>
2014-05-11 02:36:18 -0400

    new code for the reed switches

--- code/trash/ouijaPins.py
+++ code/trash/ouijaPins.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
@@ -8,29 +10,29 @@ GPIO.setup(18, GPIO.IN)
 GPIO.setup(27, GPIO.IN)
 GPIO.setup(22, GPIO.IN)
 GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(25, GPIO.IN)
 
 while True:
 
 	if(GPIO.input(4) ==1):
-		print(“OUIJA”)
+		print('OUIJA')
 
-	if(GPIO.input(17) == 0):
-		print(“H”)
+	if(GPIO.input(17) == 1):
+		print('H')
 
 	if(GPIO.input(18) ==1):
-		print(“I”)
+		print('I')
 
-	if(GPIO.input(27) == 0):
-		print(“T”)
+	if(GPIO.input(27) == 1):
+		print('T')
 
 	if(GPIO.input(22) ==1):
-		print(“L”)
+		print('L')
 
-	if(GPIO.input(23) == 0):
-		print(“E”)
+	if(GPIO.input(23) == 1):
+		print('E')
 
-	if(GPIO.input(24) == 0):
-		print(“R”)			
+	if(GPIO.input(25) == 1):
+		print('R')			
 
 GPIO.cleanup()
